973a41e9114bdda0cddcbe65dd784a2a01816c04
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-11-08 05:55:13 -0500

    checking fetch responsein console

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+let API_URL = `https://jobs.github.com/positions.json`;
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      outputText: "Placeholder text where result would show"
+    }
+  }
+
+
+  componentWillMount() {
+    fetch(`${API_URL}`, {
+      method: 'GET',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then(res => {setTimeout(console.log(`!`, res), 5000)
+      
+    }).catch(err => {
+      console.log(`This error showed up: ${err}`)
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let formData = {
+      skill: this.skill.value,
+      location: this.location.value
+    }
+    fetch(`${API_URL}?description=${formData.skill}&location=${formData.location}`, {
+      method: 'GET',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(`This error showed up: ${err}`)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <p><input type="text" size="30" ref={(input) => { this.skill = input }}
+            placeholder="Main Skill" /></p>
+          <p><input type="text" size="30" ref={(input) => { this.location = input }}
+            placeholder="Location" /></p>
+          <button>Add Item</button>
+        </form>
+
+        <div>{this.state.outputText}</div>
+      </div>
+    );
+  }
+}
+
+export default App;
